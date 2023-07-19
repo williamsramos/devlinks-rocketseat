e012b3c843f62b093e2fb97daefc00a6465fb2fe
William Ramos <ramowilliam@gmail.com>
2023-07-19 17:36:16 -0300

    adjust script

--- script.js
+++ script.js
@@ -9,15 +9,6 @@ function toggleMode() {
     } else {
         img.setAttribute('src', './assets/avatar.png');
     }
-
-    const alt = document.querySelector('#profile img alt')
-
-    if(html.classList.contains('light')) {
-        alt.setAttribute('alt' , 'Foto de William Ramos sorrindo, usando óculos escuros e camisa social, barba feita e fundo rio vermelho' )
-    }else {
-        alt.setAttribute('alt' , 'Foto de William Ramos sério, usando óculos e camisa do bahia, barba e fundo fonte nova' )
-
-    }
     
 }
 
